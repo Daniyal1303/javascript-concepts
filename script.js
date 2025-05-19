9ecaeb9ce62ed995adfa8428495f0e218a28a03b
Daniyal1303 <daniyalahmad1303@gmail.com>
2025-05-19 17:53:06 +0500

    reset try

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("Hello,World!");
 console.log("Daniyal Dev is here");
-console.log("This is a test");
 console.log("hello universe");
+console.log("bad error");
+console.log("hello multiverse");
+console.log("hello galaxy");
