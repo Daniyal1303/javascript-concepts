289d9f0c0ff11d0b0698dce0487cf4243f4038c3
Daniyal1303 <daniyalahmad1303@gmail.com>
2025-05-19 17:38:58 +0500

    restore reset changes

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ console.log("Daniyal Dev is here");
 console.log("This is a test");
 console.log("hello universe");
 console.log("bad error");
+console.log("hello multiverse");
+console.log("hello galaxy");
