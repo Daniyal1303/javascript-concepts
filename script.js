a541254b787f54efdf699560ef29ea35b62a38fb
Daniyal1303 <daniyalahmad1303@gmail.com>
2025-05-19 17:44:59 +0500

    Revert "test commit"
    This reverts commit 944198ee46b6d30f3d248bbbf502a8f2981b4ea4.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 console.log("Hello,World!");
 console.log("Daniyal Dev is here");
-console.log("This is a test");
 console.log("hello universe");
 console.log("bad error");
 console.log("hello multiverse");
